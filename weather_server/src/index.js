4c2152e27ea6826a146a920b0210706d1c903b22
Saul Rojas <luckly083@gmail.com>
2022-10-17 21:51:29 -0500

    se agregó la geolocalización en el servidor

--- weather_server/src/index.js
+++ weather_server/src/index.js
@@ -1,39 +1,106 @@
-const { createBot } = require('whatsapp-cloud-api');
-require('dotenv').config();
+const express = require("express");
+const body_parser = require("body-parser");
+const axios = require("axios");
+const cors = require('cors');
 
-(async () => {
-  try {
-    // replace the values below
-    const from = process.env.WHATSAPP_PHONE_NUMBER_ID;
-    const token = process.env.ACCESS_TOKEN;
-    const to = '51969601133';
-    const webhookVerifyToken = process.env.WEBHOOK_VERIFICATION_TOKEN;
+// modulos de comandos
 
-    // Create a bot that can send messages
-    const bot = createBot(from, token);
+require("dotenv").config();
 
-    // Send text message
-    const result = await bot.sendText(to, 'Hello world');
+// creando el servidor express
+const app = express();
+const app_port = process.env.PORT || 8000;
 
-    // Start express server to listen for incoming messages
-    // NOTE: See below under `Documentation/Tutorial` to learn how
-    // you can verify the webhook URL and make the server publicly available
-    await bot.startExpressServer({
-      webhookVerifyToken,
-    });
+// Middlewares
+app.use(body_parser.json());
 
-    // Listen to ALL incoming messages
-    // NOTE: remember to always run: await bot.startExpressServer() first
-    bot.on('message', async (msg) => {
-      console.log(msg);
 
-      if (msg.type === 'text') {
-        await bot.sendText(msg.from, 'Received your text message!');
-      } else if (msg.type === 'image') {
-        await bot.sendText(msg.from, 'Received your image!');
-      }
-    });
-  } catch (err) {
-    console.log(err);
+// to verify the callback url from dashboard side - cloud api side
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+  if (mode && token) {
+    if (mode === "subscribe" && token === process.env.WEBHOOK_TOKEN) {
+      console.log("WHATSAPP WEBHOOK SUCCESSFULLY SETUP ✅");
+      res.status(200).send(challenge);
+    } else {
+      console.log("WHATSAPP WEBHOOK FAILURE SETUP ✅");
+      res.status(403);
+    }
   }
-})();
+});
+
+// !IMPORTANTE!: esta librería con su método parseMessage sobreescribe
+// el req.body que recibimos, tener cuidado con esto
+// !IMPORTANTE!: para que los mensajes lleguen en orden debes hacer una cadena de
+// funciones asíncronas, async await
+app.post("/webhook", async (req, res) => {
+  // verificamos que hay datos disponibles
+  res.sendStatus(200).send({
+    status: "OK"
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "OK"
+  });
+});
+
+// options for good configuration
+app.options("/geolocation", cors());
+app.post("/geolocation", cors(), async (req, res) => {
+  console.log(req.body);
+  let address = encodeURIComponent(req.body.address.trim());
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GEOCODING_API_KEY}`
+
+  const response = await axios.get(url);
+
+  if (response.status !== 200) {
+    res.status(400).json({
+      status: "ERROR"
+    });
+    return;
+  }
+
+  let data = response.data;
+
+  if (data.status !== "OK") {
+    if (data.status === "ZERO_RESULTS") {
+      res.status(404).json({
+        status: "NO FOUND RESULTS"
+      });
+    } else {
+      res.status(400).json({
+        status: "ERROR"
+      });
+    }
+    return;
+  }
+
+  // ahora manejamos la respuesta, devolvemos el primer resultado
+  res.status(200).json({
+    status: "OK",
+    result: {
+      lat: data.results[0].geometry.location.lat,
+      lng: data.results[0].geometry.location.lng,
+      bounds: data.results[0].geometry.viewport
+    }
+  });
+});
+
+app.post("/weather", (req, res) => {
+  let lat = req.body.lat;
+  let lng = req.body.lng;
+  console.log("lat:", lat);
+  console.log("lng:", lng);
+  res.status(200).json({
+    status: "OK"
+  });
+});
+
+
+app.listen(app_port, () => {
+  console.log(`Listening on port ${app_port}`);
+});
